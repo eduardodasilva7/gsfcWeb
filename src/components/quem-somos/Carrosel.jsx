df7cc9856eec7343fab95375c3e7028e87e9bf13
eduardodasilva7 <eduardo12dias@yahoo.com>
2025-09-02 09:21:48 -0300

    Refactor Carrosel component and update styles for improved layout and responsiveness

--- src/components/quem-somos/Carrosel.jsx
+++ src/components/quem-somos/Carrosel.jsx
@@ -1,39 +1,20 @@
-import './QuemSomos.css'
+import './QuemSomos.css';
 import img from '../../assets/img/imgExemplo.png'
 import img2 from '../../assets/img/logoP.png'
 import img3 from '../../assets/img/logoG.png'
 
 function Carrosel() {
+
   return (
-    <div>
-      <div id="carouselExampleDark" class="carousel carousel-dark slide">
+    <div className="carousel" >
+      <div className="cartao" style={{backgroundImage: `url(${img})`}}></div>
+      <div className="cartao" style={{backgroundImage: `url(${img2})`}}></div>
+      <div className="cartao" style={{backgroundImage: `url(${img3})`}}></div>
+      <div className="cartao" style={{backgroundImage: `url(${img})`}}></div>
+      
+      
 
-      <div className="carousel-indicators">
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-        <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
-      </div>
-
-      <div className="carousel-inner">
-        <div className="carousel-item active" data-bs-interval="10000">
-          <img src={img} alt="" className="d-block w-100"/>
-        </div>
-        <div className="carousel-item" data-bs-interval="20000">
-          <img src={img2} alt="" className="d-block w-100"/>
-        </div>
-        <div className="carousel-item">
-          <img src={img3} alt="" className="d-block w-100"/>
-        </div>
-      </div>
-
-      <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-      </button>
-      <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-      </button>
-    </div>
-    </div>
+    </div> 
   )
 }
 
