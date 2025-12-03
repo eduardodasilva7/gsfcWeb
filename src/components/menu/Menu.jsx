cf751688d8f8f73a9efacc6aff97a9fd92bde812
eduardodasilva7 <eduardo12dias@yahoo.com>
2025-12-03 11:31:31 -0300

    a'

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,32 +1,70 @@
-import menuImg from '../../assets/img/menu.png';
-import './Menu.css'
+import menuImg from "../../assets/img/menu.png";
+import "./Menu.css";
+import { useNavigate } from "react-router-dom";
 
-
-function Menu(){
+function Menu() {
   const abrirMenu = () => {
-    document.querySelector('.menuItems').classList.toggle('active');
-  }
+    document.querySelector(".menuItems").classList.toggle("active");
+  };
 
-  return(
-    <div className='menu'>
-      <div className='menuImg'>
-        <img className='logo' id='logo'/>
-        <img src={menuImg} alt="" className='logomenu' onClick={abrirMenu}/>
+  const navigate = useNavigate();
+
+  const inicio = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="menu">
+      <div className="menuImg">
+        <img className="logo" id="logo" onClick={inicio} />
+        <img src={menuImg} alt="" className="logomenu" onClick={abrirMenu} />
       </div>
-      <nav> 
-        <ul className='menuItems'>
-          <li className='item'><a href="/" className='btnNav'>Início</a></li>
-          <li className='item'><a href="/#atividades" className='btnNav'>Atividades</a></li>
-          <li className='item'><a href="/luz-e-amor" className='btnNav'>Luz e Amor</a></li>
-          <li className='item'><a href="/quem-somos" className='btnNav'>Quem somos</a></li>
-          <li className="item"><a href="/relatorio" className="btnNav">Relatório</a></li>
-          <li className="item"><a href="/eventos" className="btnNav">Eventos</a></li>
-          <li className="item"><a href="/formulario" className="btnNav">Formulário</a></li>
-          <li className='item'><a href="/#doacao" className='btnNav'>Como doar</a></li>
+      <nav>
+        <ul className="menuItems">
+          <li className="item">
+            <a href="/" className="btnNav">
+              Início
+            </a>
+          </li>
+          <li className="item">
+            <a href="/#atividades" className="btnNav">
+              Atividades
+            </a>
+          </li>
+          <li className="item">
+            <a href="/luz-e-amor" className="btnNav">
+              Luz e Amor
+            </a>
+          </li>
+          <li className="item">
+            <a href="/#doacao" className="btnNav">
+              Como doar
+            </a>
+          </li>
+          <li className="item">
+            <a href="/eventos" className="btnNav">
+              Eventos
+            </a>
+          </li>
+          <li className="item">
+            <a href="/quem-somos" className="btnNav">
+              Quem somos
+            </a>
+          </li>
+          <li className="item">
+            <a href="/relatorio" className="btnNav">
+              Relatório
+            </a>
+          </li>
+          <li className="item">
+            <a href="/formulario" className="btnNav">
+              Formulário
+            </a>
+          </li>
         </ul>
       </nav>
     </div>
-  )
+  );
 }
 
 export default Menu;
