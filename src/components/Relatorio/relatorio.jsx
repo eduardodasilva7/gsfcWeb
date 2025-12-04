b8fd17114070654d57fd209d6591fbbd1db96402
dharla-bsilva <dharla.barros@gmail.com>
2025-12-04 11:16:16 -0300

    a

--- src/components/Relatorio/relatorio.jsx
+++ src/components/Relatorio/relatorio.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import './relatorio.css';
 import Menu from '../menu/Menu';
+import { Link } from "react-router-dom";
+import fundo6 from '../../assets/img/fundo6.jpg'
+
+
 
 function Relatorio() {
   return (
@@ -9,8 +13,58 @@ function Relatorio() {
 
       <main className="relatorio-container">
         <section className="relatorio-header">
+
           <h1>Relatório</h1>
           <p>Acompanhe nossos dados</p>
+
+          <section className="artigos-grid">
+
+            <div className="artigo-card">
+              <img src={fundo6} alt="anos" />
+              <h3>relatório 2020</h3>
+              <Link to="/artigos/amor">
+                <button>Saiba mais</button>
+              </Link>
+            </div>
+
+            <div className="artigo-card">
+              <img src={fundo6} alt="juventude" />
+              <h3>Relatório 2021</h3>
+              <Link to="/artigos/juventude">
+                <button>Saiba mais</button>
+              </Link>
+            </div>
+
+            <div className="artigo-card">
+              <img src={fundo6} alt="juventude" />
+              <h3>Relatório 2022</h3>
+              <Link to="/artigos/juventude">
+                <button>Saiba mais</button>
+              </Link>
+            </div>
+            <div className="artigo-card">
+              <img src={fundo6} alt="juventude" />
+              <h3>Relatório 2023</h3>
+              <Link to="/artigos/juventude">
+                <button>Saiba mais</button>
+              </Link>
+            </div>
+            <div className="artigo-card">
+              <img src={fundo6} alt="juventude" />
+              <h3>Relatório 2024</h3>
+              <Link to="/artigos/juventude">
+                <button>Saiba mais</button>
+              </Link>
+            </div>
+            <div className="artigo-card">
+              <img src={fundo6} alt="juventude" />
+              <h3>Relatório 2025</h3>
+              <Link to="/artigos/juventude">
+                <button>Saiba mais</button>
+              </Link>
+            </div>
+
+          </section>
         </section>
       </main>
     </div>
@@ -18,5 +72,4 @@ function Relatorio() {
 }
 
 export default Relatorio;
-
 
